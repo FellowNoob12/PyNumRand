97ab660cce52ba12c066bbca696786c66b6f61cc
Kyle Matthew <79827683+PewDiePieFan-Github@users.noreply.github.com>
2021-12-16 10:19:17 +0800

    Used a function instead of a loop to fix the bug.

--- numrand.py
+++ numrand.py
@@ -9,15 +9,24 @@ welcomeMessage = """
 """
 
 print(welcomeMessage)
-    
-while True:
+
+def Function():
     amount = int(input("\nHow large is the number do you want? \n"))
+    global answer
     answer = int(input("What is the number the computer randomly picked? \n"))
+    global num
     num = random.randint(0, amount)
     
-    if answer is num:
-        print("You got the correct answer!")
-        continue
+Function()
+
+if answer == num:
+    print("You got the correct answer!")
+    Function()
+else:
+    print("You got it wrong.")
+    ans = input("\nDo you want to continue? \n")
+    if ans == "No":
+        pass
     else:
-        print("You got it wrong.")
-        continue
+        Function()
+        
